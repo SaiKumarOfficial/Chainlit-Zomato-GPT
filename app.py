884eb691267c51e42c2769a3d95b9cfa8d0a180c
saikumar <sai680513@gmail.com>
2023-06-13 11:55:42 +0530

    Build a basic chatbot

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 
 import chainlit as cl
-
+from src.sys_config import system_instruction
+from src.llm import ask_order_bot,messages
 
 @cl.on_message
 def main(message: str):
     # Your custom logic goes here...
-
+    messages.append({"role":"user","content":message})
+    response = ask_order_bot(messages)
+    messages.append({"role":"assistant","content":message})
     # Send a response back to the user
     cl.Message(
-        content=f"Received: {message}",
+        content=response,
     ).send()
 
